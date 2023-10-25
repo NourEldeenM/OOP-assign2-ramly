c4cc6a5dc6904f8353ec6a1e0dbb751c90fab9c5
SaraMoh0103 <122822921+SaraMoh0103@users.noreply.github.com>
2023-10-26 01:27:03 +0300

    Create A2_S5_20221070_2.cpp

---
+++ A2_S5_20221070_2.cpp
@@ -0,0 +1,173 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string s;
+    getline(cin,s);
+    string m="",t="";
+    for (int i=0;i<s.size();i++){
+        t+=s[i];
+        if (s[i]==' '){
+            if (t=="He"){
+           size_t found = t.find("He");
+           if (found==0){
+              found+=2;
+              if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or she");
+           }
+              m+=t;
+            }
+
+            else if (t.find("he")!=std::string::npos){
+             size_t found = t.find("he");
+             if (found==0){
+             found+=2;
+             if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or she");
+             }
+              m+=t;
+
+            }
+            else if (t.find("himself")!=std::string::npos){
+             size_t found = t.find("himself");
+             if (found==0){
+              found+=7;
+              if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or herself");
+             }
+              m+=t;
+            }
+            else if (t.find("Himself")!=std::string::npos){
+              size_t found = t.find("Himself");
+              if (found ==0){
+               found+=7;
+               if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or herself");
+              }
+              m+=t;
+            }
+            else if (t.find("Him")!=std::string::npos){
+             size_t found = t.find("Him");
+             if (found ==0){
+             found+=3;
+             if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or her");
+             }
+              m+=t;
+
+            }
+            else if (t.find("him")!=std::string::npos){
+             size_t found = t.find("him");
+             if (found ==0){
+              found+=3;
+              if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or her");
+             }
+              m+=t;
+            }
+            else if (t.find("His")!=std::string::npos){
+             size_t found = t.find("His");
+             if (found ==0){
+              found+=3;
+              if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or her");
+             }
+              m+=t;
+            }
+            else if (t.find("his")!=std::string::npos){
+             size_t found = t.find("his");
+             if (found==0){
+              found+=3;
+              if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or her");
+             }
+              m+=t;
+            }
+            else {
+                m+=t;
+            }
+            t="";
+        }
+         else if (i==s.size()-1){
+       if (t=="He"){
+           size_t found = t.find("He");
+           if (found==0){
+              found+=2;
+              if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or she");
+           }
+              m+=t;
+            }
+
+            else if (t.find("he")!=std::string::npos){
+             size_t found = t.find("he");
+             if (found==0){
+             found+=2;
+             if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or she");
+             }
+              m+=t;
+
+            }
+            else if (t.find("himself")!=std::string::npos){
+             size_t found = t.find("himself");
+             if (found==0){
+              found+=7;
+              if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or herself");
+             }
+              m+=t;
+            }
+            else if (t.find("Himself")!=std::string::npos){
+              size_t found = t.find("Himself");
+              if (found ==0){
+               found+=7;
+               if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or herself");
+              }
+              m+=t;
+            }
+            else if (t.find("Him")!=std::string::npos){
+             size_t found = t.find("Him");
+             if (found ==0){
+             found+=3;
+             if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or her");
+             }
+              m+=t;
+
+            }
+            else if (t.find("him")!=std::string::npos){
+             size_t found = t.find("him");
+             if (found ==0){
+              found+=3;
+              if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or her");
+             }
+              m+=t;
+            }
+            else if (t.find("His")!=std::string::npos){
+             size_t found = t.find("His");
+             if (found ==0){
+              found+=3;
+              if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or her");
+             }
+              m+=t;
+            }
+            else if (t.find("his")!=std::string::npos){
+             size_t found = t.find("his");
+             if (found==0){
+              found+=3;
+              if (!(isalpha(t[found])) && !(isdigit(t[found])))
+              t.insert(found," or her");
+             }
+              m+=t;
+            }
+            else {
+                m+=t;
+            }
+            t="";
+    }
+    }
+    cout<<m;
+}
