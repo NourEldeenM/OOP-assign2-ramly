96ede7710a7c77be28bcb2e99c036c908d11cb9b
NOURisLIGHT <nourislight@gmail.com>
2023-11-09 17:47:41 +0200

    Anything better than nothing

---
+++ Task 3/A2_Task3_S56_20221070-20221145-20220365.cpp
@@ -0,0 +1,8 @@
+#include "headers.cpp"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+}
---
+++ Task 3/headers.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+#include "headers.h"
+using namespace std;
+
+
